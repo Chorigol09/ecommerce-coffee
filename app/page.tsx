6e630e4a3138aae8978e5459534622b529483d7d
Chorigol09 <santigil2016@gmail.com>
2024-07-18 11:40:57 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import CarouselTextBanner from "@/components/carousel-text-banner";
+import FeaturedProducts from "@/components/featured-products";
+import BannerDiscount from "@/components/banner-discount";
+import ChooseCategory from "@/components/choose-category";
+
+export default function Home() {
+  return (
+    <main>
+      <CarouselTextBanner/>
+      <FeaturedProducts/>
+      <BannerDiscount/>
+      <ChooseCategory/>
+    </main>
+  );
+}
